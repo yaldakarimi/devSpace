7993a32714af3acd0d894860407a55cdba2c66db
Yalda Karimi <yalda.karimi88@gmail.com>
2021-06-19 10:07:41 +0430

    search endpoint and cache functionality

---
+++ scripts/cache.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const path = require('path');
+const matter = require('gray-matter');
+
+function postData() {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+
+    const { data: frontMatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontMatter,
+    };
+  });
+
+  return `export const posts = ${JSON.stringify(posts)}`;
+}
+
+try {
+  fs.readdirSync('cache');
+} catch (error) {
+  fs.mkdirSync('cache');
+}
+
+fs.writeFile('cache/data.js', postData(), function (err) {
+  if (err) return console.log(err);
+});
